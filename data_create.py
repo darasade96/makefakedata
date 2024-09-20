19796f9556f9ce44e185d24be443ba2b17c59db8
Oluwadara Lasisi <oluwadara.lasisi@accenture.com>
2024-09-20 12:44:58 +0100

    uploading the code file

---
+++ data_create.py
@@ -0,0 +1,8 @@
+columns_headers = [
+    'cholesterol_level', 'diabetes_pedigree_function', 'family_diabetes_history', 'diet_type', 
+    'star_sign', 'social_media_usage', 'physical_activity_level', 'sleep_duration'
+]
+
+# dara = 'cholesterol_level, family_diabetes_history, social_media_usage, sleep_duration
+
+# irene = diabetes_pedigree_function, diet_type, physical_activity_level, 
